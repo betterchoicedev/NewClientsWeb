e79ce9c6b3301554cd213d07ac035f1d2109f6e4
Obi1kanobii <roee@betterchoice.live>
2025-12-28 12:04:01 +0200

    Rename backend.py to application.py for Azure auto-detection

--- Backend/startup.sh
+++ Backend/startup.sh
@@ -7,4 +7,4 @@ python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
 echo "Starting uvicorn..."
-python -m uvicorn backend:app --host 0.0.0.0 --port 8000
+python -m uvicorn application:app --host 0.0.0.0 --port 8000
